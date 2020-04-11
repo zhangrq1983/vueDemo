69df2410732d94faf0c6e9f66665bc77ab3a79dd
zhangrq <zhangrongianglo@163.com>
2020-04-11 23:59:40 +0800

    update

---
+++ src-base/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img class="logo" src="./assets/logo.png" alt="logo">
+    <HelloWorld/>
+  </div>
+</template>
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  components:{
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+  .logo{
+    width: 200px;
+    height: 200px;
+  }
+</style>
