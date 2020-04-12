ebc8c5c01b9bcf71470572d85691f71c052302af
zhangrq <zhangrongianglo@163.com>
2020-04-12 12:16:13 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <TodoHeader/>
+      <TodoList/>
+      <todo-footer/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TodoHeader from './components/TodoHeader'
+  import TodoList from './components/TodoList'
+  import TodoFooter from './components/TodoFooter'
+  export default {
+    components:{
+      TodoHeader,TodoList,TodoFooter
+    }
+  }
+</script>
+
+<style>
+  /*app*/
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
